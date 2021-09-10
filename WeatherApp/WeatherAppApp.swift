35c7b252be7a431ad345749ea3146095612a114b
7sodikov <78784540+7sodikov@users.noreply.github.com>
2021-09-10 11:14:37 +0500

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Utkirjon on 10/09/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
